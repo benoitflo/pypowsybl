b84d8d3eed91644dec544732bf6a1b7cbd817ced
Etienne LESOT <etienne.lesot@rte-france.com>
2022-11-14 15:09:06 +0100

    add deep removal for network elements
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- java/src/main/java/com/powsybl/python/network/NetworkCFunctions.java
+++ java/src/main/java/com/powsybl/python/network/NetworkCFunctions.java
@@ -898,4 +898,14 @@ public final class NetworkCFunctions {
         });
     }
 
+    @CEntryPoint(name = "removeDeeply")
+    public static void removeDeeply(IsolateThread thread, ObjectHandle networkHandle,
+                                    CCharPointerPointer idsPtrPtr, int idsCount, ExceptionHandlerPointer exceptionHandlerPtr) {
+        doCatch(exceptionHandlerPtr, () -> {
+            List<String> ids = toStringList(idsPtrPtr, idsCount);
+            Network network = ObjectHandles.getGlobal().get(networkHandle);
+
+        });
+    }
+
 }
